25048170c316657d0e809b8059ccd76b1181debc
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-07-21 15:45:19 +0200

    Add a step to check Rust code format in Travis-CI

--- ci/travis/script.sh
+++ ci/travis/script.sh
@@ -17,3 +17,14 @@ fi
 
 log Testing with coverage
 cargo tarpaulin -v --out Xml --ciserver travis-ci
+
+
+# --- Check code format ------------------------------------------------------
+
+# only test without coverage in nightly since the span of diagnositcs is
+# currently not for the nightly and the other channels, most likely because
+# of `proc-macro2`
+if [ "$TRAVIS_RUST_VERSION" == "nightly" ]; then
+	log Checking Rust code format with \`cargo fmt\`
+	cargo fmt -- --check
+fi
