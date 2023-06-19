c72b12e6345aa5b837577fcefacc378049266443
Martin Larralde <martin.larralde@embl.de>
2023-06-19 11:41:22 +0200

    Remove outdated Travis-CI configuration files

--- ci/travis/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -e
-
-. $(dirname $0)/functions.sh
-
-# --- Deploy crate -----------------------------------------------------------
-
-log Deploying \`blanket\` $TRAVIS_TAG
-cargo publish --manifest-path Cargo.toml --token $CRATES_IO_TOKEN
