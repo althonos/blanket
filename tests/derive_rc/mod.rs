ce23e0b6ce427e1386162b0efaa5b360afac0be5
Martin Larralde <martin.larralde@embl.de>
2023-06-19 13:13:00 +0200

    Fix test compilation for `tarpaulin`

--- tests/derive_rc/mod.rs
+++ tests/derive_rc/mod.rs
@@ -1,7 +1,6 @@
 extern crate trybuild;
 
 fn main() {
-    #[cfg(not(tarpaulin))]
     let t = trybuild::TestCases::new();
     t.compile_fail(file!().replace("mod.rs", "fails/*.rs"));
     t.pass(file!().replace("mod.rs", "successes/*.rs"));
